bcf6a6ab544226f7b86199ceab9204483b946201
Shat899 <142344295+Shat899@users.noreply.github.com>
2025-12-11 22:20:04 +0200

    control Flow, cycle and homework

--- CSharpCourseSolution/B_ControlFlow/Program.cs
+++ CSharpCourseSolution/B_ControlFlow/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Channels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CShaprCourse
 {
@@ -9,22 +10,255 @@ namespace CShaprCourse
         static void Main(string[] args) //void --> function doesn't return any value
         {
 
+            /*First two numbers - units. All subsequent numbers are calculated as the sum of the previous two
+            Task: ask the user how many Fibonacci numbers he wants to generate (calculate), and actually generate
+            (calculate). In the generation process, write the numbers to the array. After generation, 
+            display the calculated numbers.*/
 
+            int counter = new int();
+            Console.WriteLine("Enter the number of Fibonacci numbers");
+            counter = int.Parse(Console.ReadLine()); 
+            int[] numbers = new int[2 + counter];
+            numbers[0] = 1;
+            numbers[1] = 1;
+
+            for (int i = 1; i <= counter; i++)
+            {
+                numbers[i + 1] = numbers[i - 1] + numbers[i];
+            }
+            
+
+            foreach (var val in numbers)
+            {
+                Console.Write(val + " ");
+            }
+
+            Console.ReadLine();
+
+
+            /* Prompt the user for up to 10 positive integers. The user can stop accepting numbers by entering 0.
+               After the user stops accepting integers (this happens if 10 numbers have been entered, or if the user 
+               enters 0 to avoid entering all 10), calculate the average of positive integers that are multiples of three
+               and display it on the console.*/
+            bool exit = false;
+            int[] averageNumbers = new int[10];
+            int average = new int();
+            while (averageNumbers.Length <= 10)
+            {
+                Console.WriteLine("Calculate average");
+                Console.WriteLine("Input 10 integer numbers. If you want to stop before reaching 10, type 0.");
+                for (int i = 0; i < averageNumbers.Length; i++)
+                {
+                    averageNumbers[i] = int.Parse(Console.ReadLine());
+                    if (averageNumbers[i] == 0)
+                    {
+                        exit = true;
+                        break;
+                    }
+
+                }
+
+                if (exit == true || averageNumbers.Length <= 10)
+                {
+                    Console.WriteLine($"Average: {averageNumbers.Where(n => n != 0).Average()}");// LINQ method to calculate average excluding zeros
+                    break;
+                }
+            }
         }
         static void HomeWork2() //void --> function doesn't return any value
         {
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
 
-            int firstNumber = new int();
-            int secondNumber = new int();
-            string result = new string("");
-            Console.WriteLine("Please Enter 2 integer numbers");
-            firstNumber = int.Parse(Console.ReadLine());
-            secondNumber = int.Parse(Console.ReadLine());
+                Console.Write(numbers[i] + " "); // Print numbers in a single line
 
-            result = firstNumber > secondNumber ? $"First number {firstNumber} is begger then second Number {secondNumber}" :
-                                                  $"Second number {secondNumber} is begger then first Number {firstNumber}";
-            Console.WriteLine(result);
+            }
+            
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+
+                Console.Write(numbers[i] + " "); // Print numbers in a single line in reverse order
+            }
+            
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0) // Check if the number is even
+                {
+
+                    Console.Write(numbers[i] + " "); // Print even numbers in a single line
+
+                }
+                
+
+            }
+            
+
+            foreach (int val in numbers)
+            {
+
+                Console.Write(val + " "); // Print numbers using foreach loop without index
+            }
+            
+
+            for (int i = 0; i < numbers.Length - 1; i++) // Find pairs that sum to zero
+            {
+                for (int j = i + 1; j < numbers.Length; j++) // Start j from i + 1 to avoid duplicate pairs and self-pairing using cicles in cicles
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0) // Check if the sum of the pair is zero
+                    {
+                        Console.WriteLine($"Pair found: {atI}, {atJ}"); // Print the pair
+                    }
+                    
+                }
+                
+            }
+            
+
+            for (int i = 0; i < numbers.Length - 2; i++) // Find pairs that sum to zero
+            {
+
+                for (int j = i + 1; j < numbers.Length - 1; j++) // Start j from i + 1 to avoid duplicate pairs and self-pairing using cicles in cicles
+                {
+                    for (int k = j + 1; k < numbers.Length; k++) // Start k from j + 1 to avoid duplicate pairs and self-pairing using cicles in cicles
+                    {
+                        int atI1 = numbers[i];
+                        int atJ2 = numbers[j];
+                        int atK = numbers[k];
+                        if (atI1 + atJ2 + atK == 0) // Check if the sum of the pair is zero
+                        {
+                            Console.WriteLine($"Triplet found: {atI1}, {atJ2}, {atK}"); // Print the pair
+                        }
+                        ;
+                    }
+                    ;
+
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+                }
+                
+
+            }
+            
+
+            // while: Checks the condition before executing the code block (Pre-test loop).
+            // do -while: Executes the code block once and then checks the condition(Post-test loop).
+
+            int age = new int();
+            while (age < 18) // Check if the number is even
+            {
+                Console.WriteLine("Please enter your age: ");
+                age = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Good !!!");
+
+            do // Check if the number is even
+            {
+                Console.WriteLine("Please enter your age: ");
+                age = int.Parse(Console.ReadLine());
+
+            } while (age < 18); // Check if the number is even
+
+            Console.WriteLine("Good !!!");
+
+            int[] intNumbers = { 0, 3, 2, 1, 5, 4, 6, 7, 8, 9 };
+            char[] leters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+
+            for (int i = 0; i < intNumbers.Length; i++)
+            {
+                Console.WriteLine($"Number={numbers[i]};");
+
+                for (int j = 0; j < leters.Length; j++)
+                {
+                    if (intNumbers[i] == j)
+                        break; // exit the inner loop when condition is met
+                    Console.WriteLine($"Leter={leters[j]};");
+                };
+            };
+
+            foreach (int n in intNumbers)
+            {
+                if (n % 2 != 0)
+                    continue; //The continue statement is used to skip the rest of the current iteration and jump immediately to the next evaluation of the loop's condition.
+                Console.WriteLine($"Even Number={n};");
+            }
+            
+
+            Console.WriteLine("How long have you are married?");
+            int weaddingYears = int.Parse(Console.ReadLine());
+            string str = string.Empty;
+
+            switch (weaddingYears)
+            {                             // switch statement
+                case 1:
+                    str = "You are newlyweds";
+                    break;
+                case 5:
+                    str = "You have a small family";
+                    break;
+                case 10:
+                    str = "You are an experienced couple";
+                    break;
+                case 20:
+                    str = "You are an example for others";
+                    break;
+                default:
+                    str = "You have a good relationship";
+                    break;
+            }
+            
+            Console.WriteLine(str);
+
+            Console.WriteLine("Please enter month number (1-12): ");
+            int month = new int();
+            string season = string.Empty;
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    season = "Winter";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "Spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "Summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "Autumn";
+                    break;
+                default:
+                    season = "Invalid month number";
+                    break;
+            }
+            
+        }
+        static void ControlFlowShowcase() //void --> function doesn't return any value
+        {
+         int firstNumber = new int();
+         int secondNumber = new int();
+         string result = new string("");
+         Console.WriteLine("Please Enter 2 integer numbers");
+         firstNumber = int.Parse(Console.ReadLine());
+         secondNumber = int.Parse(Console.ReadLine());
+
+         result = firstNumber > secondNumber ? $"First number {firstNumber} is begger then second Number {secondNumber}" :
+                                               $"Second number {secondNumber} is begger then first Number {firstNumber}";
+         Console.WriteLine(result);
         }
         static void  ControlFlowDeom() //void --> function doesn't return any value
         {
@@ -66,12 +300,6 @@ namespace CShaprCourse
 
         }
 
+    };
 
-
-
-
-
-    }
-
-
-}
+};
