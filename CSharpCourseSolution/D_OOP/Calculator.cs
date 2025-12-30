ac5b5d61763149f34815031b8eeadad0adc5bc91
Shat899 <142344295+Shat899@users.noreply.github.com>
2025-12-30 12:41:30 +0200

    Method overloading example

---
+++ CSharpCourseSolution/D_OOP/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_OOP
+{
+    public class Calculator
+    {
+        // Method overloading example
+        public double CalcTriangleSquareby(double ab, double bc, double ac)  // method returns doble value, and cames with three parameters
+        {
+            
+           double p = (ab + bc + ac) / 2;
+           return Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac));
+
+        }
+
+        public double CalcTriangleSquare(double b, double h)             // method returns doble value, and cames with two parameters
+        {
+            return 0.5 * b * h;
+        }
+    }
+}
--- CSharpCourseSolution/D_OOP/Program.cs
+++ CSharpCourseSolution/D_OOP/Program.cs
@@ -7,8 +7,9 @@ namespace D_OOP
     {
         static void Main(string[] args) //void --> function doesn't return any value
         {                                            //"exmplare"
-            Character c = new Character(); // Create an instance of Character class "exmplare"
-            c.Hit(10);                     // Call the Hit method on the Character instance
-            Console.WriteLine(c.Health);   // Print the current health of the character with property
+            //Character c = new Character(); // Create an instance of Character class "exmplare"
+            //c.Hit(10);                     // Call the Hit method on the Character instance
+            //Console.WriteLine(c.Health);   // Print the current health of the character with property
+
         }
 }   }
